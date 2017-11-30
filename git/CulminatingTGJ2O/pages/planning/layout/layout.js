9f616f78cd281b38e007f2dc92550feb10d89631
Michael Pu <michael.pu123@gmail.com>
2017-11-29 22:32:40 -0500

    added git projects

---
+++ git/CulminatingTGJ2O/pages/planning/layout/layout.js
@@ -0,0 +1,10 @@
+var content = [];
+
+//when document is ready
+$(document).ready(pageReady);
+
+//page ready
+function pageReady() {
+    $("#planning").addClass("w3-green");
+    $("#layout").addClass("w3-green");
+}
